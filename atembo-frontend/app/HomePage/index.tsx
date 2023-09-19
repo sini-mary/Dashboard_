b5dcfe5c6fccfc9e6bf0e2ea457db265195fd2ff
kivuvarosekivuvan <rosekivuvan@gmail..com>
2023-09-19 16:54:32 +0300

    Added the dashboard homepage

--- atembo-frontend/app/HomePage/index.tsx
+++ atembo-frontend/app/HomePage/index.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 const HomePage: React.FC = () => {
   return (
     <div className="flex h-screen bg-white-500">
-        <p className="perform text-green-800 font-bold text-4xl ml-[450px]">PERFORMANCE OVERVIEW</p>   
-        <div className="flex justify-center ml-[-500px] mt-7 pr-[100px]">
+        <p className="perform text-green-800 font-bold text-4xl ml-[500px] w-[100%]">PERFORMANCE OVERVIEW</p>   
+        <div className="flex justify-center ml-[-2330px] mt-[100px] pr-[100px]">
         <div className="square1 w-[22%] h-64 bg-[#2CB908]  ml-[590px]">
             <p className="words text-2xl  text-center mt-10">Number Of New Clients</p>
             <p className="numbers text-7xl text-center mt-10">210</p>
@@ -18,106 +18,10 @@ const HomePage: React.FC = () => {
             <p className=" words text-2xl  text-center mt-10">Number of Active Users</p>
             <p className=" numbers text-7xl text-center mt-10">110</p>
           </div>
-</div>
-<div><h1 className="ml-[300px] text-2xl text-black-500">System usage</h1>
-</div>
+          </div>
 
-      <style>
-        {`
-          .text-green-500:hover {
-            color: white !important;
-          }
-          .hover\:px-2:hover {
-            padding-left: 0.75rem !important;
-            padding-right: 0.75rem !important;
-          }
-          .hover\:bg-[#2CB908] :hover {
-            background-color: #2CB908;
-          }
-          
-          @media screen and (max-width: 768px) {
-            .navbar {
-              flex-direction: column;
-              width: 40%; 
-            }
-            .dashboard{
-              font-size:14px;
-              margin-left:5px;
-            }
-            .list{
-              font-size:14px;
-              margin-left:5px;
-            }
-            .analysis{
-              font-size:14px;
-              margin-left:5px;
-            }
-            .navbar ul li {
-              display:flex;
-            }
-         
-            .navbar .ml-10 {
-              margin-left: 0.5rem; /* Adjust the margin as needed */
-            }
-            .logo{
-              width:150px;
-              height:15vh;
-            }
-            .text-green-500:hover {
-              color: white !important;
-            }
-            .hover\:px-2:hover {
-              padding-left: 0.55rem !important;
-              padding-right: 0.5rem !important;
-            }
-            .hover\:bg-[#2CB908] :hover {
-              background-color: #2CB908;
-            }
-            .square1{
-              width:200px;
-              height:100px;
 
-            }
-            .numbers{
-              font-size:20px;
-              margin-top:-10px;
-
-            }
-            .square2{
-              margin-top:140px;
-              width:200px;
-              height:100px;
-            }
-            .square3{
-              margin-top:265px;
-              width:200px;
-              height:100px;
-            }
-            .words{
-              font-size:15px;
-              margin-top:10px;
-            }
-            .perform {
-              font-size:20px;
-              width:20px;
-              margin-left:30px;
-              margin-top:-30px;
-            }
-       
-
-          }
-          
-          /* Media query for even smaller screens (e.g., mobile devices) */
-          @media screen and (max-width: 480px) {
-            .navbar li {
-              width: 100%;
-            }
-          }
-          
-
-          
-        `}
-      </style>
+    
     </div>
   );
 };
