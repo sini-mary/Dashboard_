6b4e376656ef722491e241dffdeeee2b92fb847e
Lado-Gloria <glorialadoo@gmail.com>
2023-09-20 16:38:40 +0300

    added the changes in login page

--- atembo-frontend/app/components/Login/page.tsx
+++ atembo-frontend/app/components/Login/page.tsx
@@ -21,6 +21,7 @@ function LoginPage() {
   return (
     <div className="flex justify-center items-center h-screen">
       <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <h1 className="text-green-700 text-4xl mb-4 font-bold">LOGIN TO DASHBOARD</h1>
         <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
             User Name
@@ -29,10 +30,11 @@ function LoginPage() {
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="username"
             type="username"
-            placeholder="UserName"
+            placeholder=""
             name="username"
             value={formData.username}
             onChange={(event) => handleChange('username', event.target.value)}
+            style={{ width: '501px', height: '56px', backgroundColor: '#ECE2E2', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.4)' }}
           />
         </div>
         <div className="mb-6">
@@ -43,15 +45,16 @@ function LoginPage() {
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="password"
             type="password"
-            placeholder="Password"
+            placeholder=""
             name="password"
             value={formData.password}
             onChange={(event) => handleChange('password', event.target.value)}
+            style={{ width: '501px', height: '56px', backgroundColor: '#ECE2E2', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.4)' }}
           />
         </div>
         <div className="flex items-center justify-between">
           <button
-            className="bg-green-500 hover:bg-green-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="bg-green-700 hover:bg-green-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="submit"
           >
             Login
--- atembo-frontend/app/components/register/page.tsx
+++ atembo-frontend/app/components/register/page.tsx
@@ -1,4 +1,6 @@
 'use client'
+
+
 import React, { useState } from 'react';
 
 function RegistrationPage() {
@@ -19,78 +21,84 @@ function RegistrationPage() {
   const handleSubmit = () => {
     console.log('Registration data:', formData);
   };
-
   return (
-    <div className="flex justify-center items-center h-screen">
-      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor=" firstName">
-          First Name
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id=" firstName"
-            type="text"
-            placeholder=" firstName"
-            name=" firstName"
-            value={formData. firstName}
-            onChange={(event) => handleChange(' firstName', event.target.value)}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor=" lastName">
-          Last Name
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="lastName"
-            type=" lastName"
-            placeholder=" lastName"
-            name="lastName"
-            value={formData.email}
-            onChange={(event) => handleChange('lastName', event.target.value)}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="userName">
-          User Name
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="userName"
-            type="userName"
-            placeholder="user Name"
-            name="userName"
-            value={formData.userName}
-            onChange={(event) => handleChange('userName', event.target.value)}
-          />
-        </div>
-        <div className="mb-6">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-          Email
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="email"
-            type="email"
-            placeholder="email"
-            name="email"
-            value={formData.email}
-            onChange={(event) => handleChange('email', event.target.value)}
-          />
-        </div>
-        <div className="flex items-center justify-between">
-          <button
-            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="submit"
-          >
-            Register
-          </button>
-        </div>
-        <div className="text-center mt-4">
-          Already have an account? <a href="#">Login</a>
-        </div>
-      </form>
+    <div className="flex justify-center items-center h-screen bg-gray-200">
+      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <h1 className="text-green-700 text-4xl mb-4 font-bold">REGISTER TO DASHBOARD</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="firstName">
+              First Name
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="firstName"
+              type="text"
+              placeholder=""
+              name="firstName"
+              value={formData.firstName}
+              onChange={(event) => handleChange('firstName', event.target.value)}
+              style={{ width: '501px', height: '56px', backgroundColor: '#ECE2E2', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.4)' }}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="lastName">
+              Last Name
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="lastName"
+              type="text"
+              placeholder=""
+              name="lastName"
+              value={formData.lastName}
+              onChange={(event) => handleChange('lastName', event.target.value)}
+              style={{ width: '501px', height: '56px', backgroundColor: '#ECE2E2', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.4)' }}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="userName">
+              User Name
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="userName"
+              type="text"
+              placeholder=""
+              name="userName"
+              value={formData.userName}
+              onChange={(event) => handleChange('userName', event.target.value)}
+              style={{ width: '501px', height: '56px', backgroundColor: '#ECE2E2', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.4)' }}
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              placeholder=""
+              name="email"
+              value={formData.email}
+              onChange={(event) => handleChange('email', event.target.value)}
+              style={{ width: '501px', height: '56px', backgroundColor: '#ECE2E2', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.4)' }}
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-green-700 hover:bg-green-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Register
+            </button>
+          </div>
+          <div className="text-center mt-4">
+            Already have an account? <a href="#">Login</a>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
