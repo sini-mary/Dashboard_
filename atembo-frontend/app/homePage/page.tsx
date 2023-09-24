953fa21597a38cd89f0759ac90c0a206ea633961
sini-mary <marysnaidaa@gmail.com>
2023-09-24 15:11:47 +0300

    Added active buttons

--- atembo-frontend/app/homePage/page.tsx
+++ atembo-frontend/app/homePage/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import "tailwindcss/tailwind.css";
 import React, { useEffect, useState } from 'react';
-import Graph from "@/app/Graph";
+import Graph from "@/app/Components/Graph";
 import { FaUserPlus, FaDesktop, FaUser } from 'react-icons/fa';
 import Recents from "@/app/recents";
 import { getDevices,getUsers } from "@/app/Utilities/utils";
