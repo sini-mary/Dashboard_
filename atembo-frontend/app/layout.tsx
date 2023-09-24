2d34d3109621d33bc61460e6116da0ac39a0615b
sini-mary <marysnaidaa@gmail.com>
2023-09-24 15:35:55 +0300

    Added active buttons

--- atembo-frontend/app/layout.tsx
+++ atembo-frontend/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Sidebar from './Sidebar'
+import Sidebar from './Components/Sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
