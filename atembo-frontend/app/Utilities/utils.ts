a8d63f6633ae3798e49338e292ff43245f0c2194
kivuvarosekivuvan <rosekivuvan@gmail..com>
2023-09-28 21:59:04 +0300

    Intergrated the overviews on homepage

--- atembo-frontend/app/Utilities/utils.ts
+++ atembo-frontend/app/Utilities/utils.ts
@@ -1,27 +1,45 @@
-export const getDevices= async()=>{
+
+export const getFlowrate = async()=>{
+    const url = './api/get-flowrate';
+
     try{
-        const response= await fetch(`/mysystem-url`,{
-            method:'GET',
-        });
-        const result= await response.json();
-        return result;
-    } 
-    catch(error){
-        return error
+        const response = await fetch(url);
+        const result = await response.json();
+        return result
     }
+    catch(error:any){
+        return error.message
+    }
+
 }
 
 
+export const getUser= async()=>{
+    const url = './api/get-users';
 
-export const getUsers= async()=>{
     try{
-        const response= await fetch(`/myusers-url`,{
-            method:'GET',
-        });
-        const result= await response.json();
-        return result;
-    } 
-    catch(error){
-        return error
+        const response = await fetch(url);
+        const result = await response.json();
+        return result
     }
+    catch(error:any){
+        return error.message
+    }
+
+}
+
+
+export const getTemp= async()=>{
+    const url = './api/get-temp';
+
+    try{
+        const response = await fetch (url);
+        const result = await response.json();
+        return result
+    }
+    catch(error:any){
+        return error.message
+
+    }
+
 }
